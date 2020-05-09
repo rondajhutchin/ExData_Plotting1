91a7d69408efa867c0480300be1c8fc5e1e6534c
rondajhutchin <rondahutchin@prodigy.net>
2020-05-09 17:17:27 -0400

    Commit

--- plot3.R
+++ plot3.R
@@ -18,5 +18,5 @@ with(subPowerData,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"
 legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
 ## Saving to file plot3.png
-dev.copy(png, file="plot3.png", height=480, width=480)
+dev.copy(png, file="plot3.png", height=480, width=580)
 dev.off()
